aaa0753d3c73354403f45ab931234ae39190e159
ArijBHA <arij.belhajamor@esprit.tn>
2022-11-10 20:56:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from "react";
+import {Routes,Route}from 'react-router-dom';
+import NavBar from './components/NavBar';
+import UserList from './components/UserList';
+import Details from './components/Details'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <h4 className='note'> NB :MONSIEUR CLIQUER SUR LE FLECHE SITUER DANS LA CARD POUR + DE DETAILS </h4>
+      <Routes>
+        <Route path='/' element={< UserList />} />
+        <Route path='Details/:id' element={<Details />} />      
+      </Routes>
+      <br/>
+
     </div>
   );
 }
